d7454471e7137f2eb5bb2395ee5a355da355a2b9
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-03-07 17:08:54 -0800

    Create sample books and add them to library

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ Book.prototype.toggleIsRead = function () {
 function addBookToLibrary(book) {
 	library.push(book);
 }
+
+const book1 = new Book("Book1", "Author1", 20, true);
+const book2 = new Book("Book2", "Author2", 40, false);
+const book3 = new Book("Book3", "Author3", 60, true);
+const book4 = new Book("Book4", "Author4", 80, false);
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+addBookToLibrary(book4);
