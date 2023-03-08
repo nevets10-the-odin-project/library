e4499079259668e6c4ed49c17c91a3643ba81648
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-03-07 19:44:06 -0800

    Add displayLibrary function

--- script.js
+++ script.js
@@ -40,3 +40,42 @@ cancelNewBtn.addEventListener("click", cancelNewBookDialog);
 function cancelNewBookDialog() {
 	newBookDialog.close();
 }
+
+function displayLibrary() {
+	const currentLibrary = document.querySelector(".library-list");
+
+	const newLibrary = document.createElement("div");
+	newLibrary.classList.add("library-list");
+
+	library.forEach((book, index) => {
+		const bookCard = document.createElement("div");
+		bookCard.classList.add("book-card");
+		bookCard.setAttribute("data-library-index", index);
+
+		const bookTitle = document.createElement("h3");
+		bookTitle.classList.add("title");
+		bookTitle.textContent = book.title;
+		bookCard.appendChild(bookTitle);
+
+		const bookAuthor = document.createElement("p");
+		bookAuthor.classList.add("author");
+		bookAuthor.textContent = `written by ${book.author}`;
+		bookCard.appendChild(bookAuthor);
+
+		const bookPageCount = document.createElement("p");
+		bookPageCount.classList.add("page-count");
+		bookPageCount.textContent = `Number of pages: ${book.pageCount}`;
+		bookCard.appendChild(bookPageCount);
+
+		const bookReadStatus = document.createElement("p");
+		bookReadStatus.classList.add("status");
+		bookReadStatus.textContent = `${book.isRead ? "read" : "unread"}`;
+		bookCard.appendChild(bookReadStatus);
+
+		newLibrary.appendChild(bookCard);
+	});
+
+	currentLibrary.replaceWith(newLibrary);
+}
+
+displayLibrary();
