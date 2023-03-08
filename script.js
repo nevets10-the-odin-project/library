cc8fcd3813158a60f78624ead607c76b346588e9
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-03-07 16:50:08 -0800

    Create library array and book constructor

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("BOOKS!");
+const library = [];
+
+function Book(title, author, pageCount, isRead) {
+	this.title = title;
+	this.author = author;
+	this.pageCount = pageCount;
+	this.isRead = isRead;
+}
